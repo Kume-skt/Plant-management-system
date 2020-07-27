af1c05575bfe3e6a666f11406c4a51a3baac7558
Kume-skt <kumeta@cps.im.dendai.ac.jp>
2020-07-27 18:58:23 +0900

    create graph
    plant_graphでメモリや単位渡すデータを選別してgraphでレンダリング

--- plantsystem/src/component/graph.tsx
+++ plantsystem/src/component/graph.tsx
@@ -1,15 +1,31 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+import ColorC from "./Data_processing/ColorChange"
+import e from 'express';
 
 interface get_data {
     plant_Data: any,
     leftGraph_DataName: string[],
     rightGraph_DataName: string[],
+    left_Scale:number[],
+    right_Scale:number[],
+    left_unit:string,
+    right_unit:string,
 }
 
 export default class Graph extends React.Component<get_data, any>  {
+    graph_DataName_List: string[];
+    color: string[];
     constructor(props: any) {
         super(props);
         this.state = {};
+    
+        this.color = []
+        this.graph_DataName_List = []
     }
     // データを親から取得
     componentWillReceiveProps(nextpro: any) {
@@ -17,12 +33,93 @@ export default class Graph extends React.Component<get_data, any>  {
             this.setState({ ...this.state, [data]: nextpro[data] })
         }
         );
-    }
-    render() {
-        console.log(this.state);
 
+    }
+    componentDidUpdate() {
+        console.log("yobaareta");
+        console.log(this.state);
+        this.graph_DataName_List = this.state.leftGraph_DataName.concat(this.state.rightGraph_DataName)
+
+        this.graph_DataName_List.map((dataName: string) => (
+            this.color.push(ColorC().hslToHex(360 * (this.graph_DataName_List.lastIndexOf(dataName) + 1) / this.graph_DataName_List.length
+                , 100, 35))
+        ))
+    }
+    // componentDidMount() {
+    //     setInterval(this.tets,1000);
+    // }
+    // tets() {
+    //         console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    // }
+    render() {
+        console.log("確認");
+        
+        if (this.graph_DataName_List.length==0 ) {
+            console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            return (<p>お待ち</p>)
+        } else{
+        
         return (
-            <p> Graph</p >
-        )
+            // <p>test</p>
+            <div className="graph">
+                <LineChart
+                    width={window.innerWidth * 0.7}
+                    height={300}
+                    data={this.state.plant_Data}
+                    margin={{
+                        top: 5,
+                        right: window.innerWidth * 0.11,
+                        left: 0,
+                        bottom: 15
+                    }
+
+                    }
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis
+                        dataKey="DATE"
+                    />
+                    <YAxis
+                        orientation="left"
+                        yAxisId="leftYAxis"
+                        domain={['dataMin', 'dataMax']}
+                        ticks={this.state.left_Scale} // Y軸に表示する温度
+                        unit={this.state.left_unit} // Y軸の単位
+                    />
+                    <YAxis
+                        orientation="right"
+                        yAxisId="rightYAxis"
+                        domain={['dataMin', 'dataMax']}
+                        ticks={this.state.right_Scale} // Y軸に表示する温度
+                        unit={this.state.right_unit} // Y軸の単位
+                    />
+                    <Tooltip />
+                    <Legend />
+                    {
+                        
+                        this.state.leftGraph_DataName.map((dataName: string) => {
+                        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                        return (<Line
+                        type="monotone"
+                        dataKey={dataName}
+                        stroke={this.color[this.graph_DataName_List.lastIndexOf(dataName)]}
+                        yAxisId='leftYAxis' />
+                        )
+                    })
+                    }
+                    {this.state.rightGraph_DataName.map((dataName: string) => {
+                            return (
+                                <Line
+                                    type="monotone"
+                                    dataKey={dataName}
+                                    stroke={this.color[this.graph_DataName_List.lastIndexOf(dataName)]}
+                                    yAxisId='rightYAxis' />
+
+                            )
+                        })}
+                </LineChart>
+            </div>
+            )
+        }
     }
 }
--- plantsystem/src/page/plant_graph.tsx
+++ plantsystem/src/page/plant_graph.tsx
@@ -14,10 +14,27 @@ class classname extends React.Component<any, any> {
     componentWillMount() {
         this.props.get_plantData();
     }
+
     render() {
         return (
-            <Graph plant_Data={this.props.plantD} leftGraph_DataName={[]} rightGraph_DataName={[]} />
-        )
+            <div>
+                <Graph plant_Data={this.props.plantD}
+                    leftGraph_DataName={["soil_value", "humidity"]}
+                    rightGraph_DataName={[]}
+                    left_Scale={[0, 20, 40, 60, 80, 100]}
+                    right_Scale={[]}
+                    left_unit="%"
+                    right_unit=""
+                />
+                <Graph plant_Data={this.props.plantD}
+                    leftGraph_DataName={["water"]}
+                    rightGraph_DataName={["Temperature","HeatIndex"]}
+                    left_Scale={[0, 2, 4, 6, 8, 10]}
+                    right_Scale={[0, 10, 20, 30,40]}
+                    left_unit="cm"
+                    right_unit="℃"
+                />
+            </div>)
     }
 }
 function mapStateToProps(state: any) {
