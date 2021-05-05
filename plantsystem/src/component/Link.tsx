f414f0ff163e6e8a1cf968c1a880554cbe98f258
Kume-skt <56424792+Kume-skt@users.noreply.github.com>
2021-05-06 00:31:01 +0900

    Mac

--- plantsystem/src/component/Link.tsx
+++ plantsystem/src/component/Link.tsx
@@ -1,37 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { css } from "emotion";
-import "../page/css/base.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import { css } from "emotion";
+import '../page/css/base.css';
 export default class Links extends React.Component {
-  nav_css: string;
+  // nav_css: string;
   constructor(props: any) {
     super(props);
     this.state = {};
-    this.nav_css = css({
-      textDecoration: "none",
-      color: "rgb(0, 0, 0)",
-      fontWeight: "bold",
-      listStyle: "none",
-      "@media (max-width: 1000px)": {
-        display: "inline",
-        backgroundColor: "red",
-      },
-    });
+    // this.nav_css = css({
+    //   textDecoration: "none",
+    //   color: "rgb(0, 0, 0)",
+    //   fontWeight: "bold",
+    //   listStyle: "none",
+    //   "@media (max-width: 1000px)": {
+    //     display: "inline",
+    //     backgroundColor: "red",
+    //   },
+    // });
   }
   render() {
     return (
-      <div id="nav">
+      <div id='nav'>
         <ul>
-          <Link to="/">
+          <Link to='/'>
             <li>Home</li>
           </Link>
-          <Link to="/test">
+          <Link to='/test'>
             <li>植物一覧</li>
           </Link>
-          <Link to="/Plant_observation">
+          <Link to='/Plant_observation'>
             <li>植物観察</li>
           </Link>
-          <Link to="/Plant_graph">
+          <Link to='/Plant_graph'>
             <li>植物情報</li>
           </Link>
         </ul>
